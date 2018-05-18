f5b91abc78210fd6106911216d95505673fd6e0d
AyanaZaire <ayanazaire@gmail.com>
2018-05-18 04:52:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(Ayana){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(Ayana, Buttercup){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(Birthday, 24){
+  return `It's ${days} days until ${holiday}!`
+}
